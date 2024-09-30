05737b42bfdfdf20fb23e009c1fdb9abb6287bf4
karla.aquino <karlaaquino371@gmail.com>
2024-09-30 14:46:44 -0600

    Creación de  arreglos de estudiante

---
+++ Estudiante.java
@@ -0,0 +1,72 @@
+public class Estudiante {
+    private String matricula;
+    private String nombre;
+    private double estructuras;
+    private double evaluacion;
+    private double ingles;
+    private double promedio;
+
+
+
+    //constructor 
+    public Estudiante(String m, String n){
+        //El objetivo es construir e inicializar el objeto
+        this.matricula = m;
+        this.nombre = n;    
+    }
+
+    //Getters y Setters
+
+    //Get: Método constructor
+    //Set: Método modificador
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getEstructuras() {
+        return estructuras;
+    }
+    public void setEstructuras(double estructuras) {
+        this.estructuras = estructuras;
+    }
+
+    public double getEvaluacion() {
+        return evaluacion;
+    }
+    public void setEvaluacion(double evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public double getIngles() {
+        return ingles;
+    }
+    public void setIngles(double ingles) {
+        this.ingles = ingles;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio() {
+        this.promedio = (this.estructuras + this.evaluacion + this.ingles) / 3;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante [matricula=" + matricula + ", nombre=" + nombre + ", promedio=" + promedio + "]";
+    }
+
+    
+}
--- PromedioGrupo.java
+++ PromedioGrupo.java
@@ -7,11 +7,46 @@ public class PromedioGrupo {
  * El tamaño del grupo es N
  * Las calificaciones se leen y se almacenan en un arreglo de tamaño n*
  */
+
+    public static Estudiante[] estudiantes;
+
+     //Para lectura de datos
+    public static BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+    public static String entrada;
+
+
+    public static void leer_estudiantes()throws IOException{
+        String matricula, nombre;
+        for(int i = 0; i < estudiantes.length; i++ ){
+            System.out.println("------------------------------");
+            System.out.println("Informacion del estudiante [" + (i+0) + "]");
+            System.out.println("Matricula: ");
+            entrada = bufer.readLine();
+            matricula = entrada;
+            System.out.println("Nombre: ");
+            entrada = bufer.readLine();
+            nombre = entrada;  
+            Estudiante e = new Estudiante(matricula, nombre);
+            System.out.println("Escriba calificacion de Estructura de Datos: ");
+            entrada = bufer.readLine();
+            e.setEstructuras(Double.parseDouble(entrada));
+            System.out.println("Escriba calificacion de Evaluación: ");
+            entrada = bufer.readLine();
+            e.setEvaluacion(Double.parseDouble(entrada));
+            System.out.println("Escriba calificacion de Inglés: ");
+            entrada = bufer.readLine();
+            e.setIngles(Double.parseDouble(entrada));
+            e.setPromedio();
+            estudiantes[i] = e;//Agregamos un objeto estudiante al arreglo
+        }
+    }
     public static void main(String[] args) throws IOException {
         int N; //El tamaño del arreglo
-        //Para lectura de datos
-        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
-        String entrada;
+       
+        //Arreglo de un Tipo de Dato Abstracto (TDA)
+
+
+        //Arrelo Tipo de Dato Primitivo(double)
         double [] calificaciones;//Arreglo de calificaciones
         double suma = 0;//para sumas calificaciones 
         double promedio; //El promedio del grupo
@@ -21,16 +56,21 @@ public class PromedioGrupo {
         entrada = bufer.readLine();
         N = Integer.parseInt(entrada);
 
+        //Construyendo el arreglo de estudiantes
+        estudiantes = new Estudiante[N];
+
         //Construimos el arreglo de calificaciones 
         calificaciones = new double[N];
-        //Leer las calificaciones de cada alumno
-        for( int i = 0; i < calificaciones.length; i++){
-            System.out.println("Escribas las calificaciones del alumno:" + (i+1) + ": ");
-            entrada = bufer.readLine();
-            calificaciones[i] = Double.parseDouble(entrada);
-            suma += calificaciones[i];
-        }
-         promedio = suma / N;
+        // //Leer las calificaciones de cada alumno
+        // for( int i = 0; i < calificaciones.length; i++){
+        //     System.out.println("Escribas las calificaciones del alumno:" + (i+1) + ": ");
+        //     entrada = bufer.readLine();
+        //     calificaciones[i] = Double.parseDouble(entrada);
+        //     suma += calificaciones[i];
+        // }
+        //  promedio = suma / N;
+
+
          System.out.println("Las calificaciones del grupo son:");
          for(int i = 0; i < calificaciones.length; i++){
             System.out.println(calificaciones[i]);
@@ -39,3 +79,4 @@ public class PromedioGrupo {
 
     }
 }
+
